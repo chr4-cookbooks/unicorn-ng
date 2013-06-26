f6969bacbbcc1359b017ea046a24e214acfb6478
Chris Aumann <me@chr4.org>
2013-06-26 19:14:21 +0200

    add documentation

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,9 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures unicorn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe           'unicorn-ng', 'Configures unicorn.rb and sets up a service according to attributes'
+recipe           'unicorn-ng::install', 'Installs bundler and unicorn using rubygems'
+
+supports         'ubuntu', '>= 10.04'
+supports         'debian', '>= 6.0'
